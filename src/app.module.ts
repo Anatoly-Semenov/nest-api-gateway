b6922d15ddf6c3b040cb542f9357740d2592655b
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-03-29 17:02:10 +0300

    Delete app controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthServiceController } from './auth-service/auth-service.controller';
 import { UserServiceController } from './user-service/user-service.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, AuthServiceController, UserServiceController],
-  providers: [AppService],
+  controllers: [AuthServiceController, UserServiceController],
+  providers: [],
 })
 export class AppModule {}
